2cfe2304646db20ff9b678a950fb544831334fd7
Matheus Ribeiro <email1001utilidades@gmail.com>
2021-12-07 11:14:22 -0300

    Alterado campo de senha para trocar os digitos por *

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,7 +4,8 @@ import {
     Text, 
     Image,
     StatusBar,
-    TextInput
+    TextInput,
+    Alert
 } from 'react-native'
 
 import 'react-native-gesture-handler';
@@ -21,6 +22,9 @@ import { styles } from "./styles"
 type createAccountScreenProp = StackNavigationProp<RootStackParamList, 'CreateAccount'>;
 
 export function SignIn(){
+
+    const [ cpf, setCPF ] = React.useState('')
+    const [ password, setPassword ] = React.useState('')
 
     const navigation = useNavigation<createAccountScreenProp>();
 
@@ -44,14 +48,33 @@ export function SignIn(){
 
             <View style={styles.content}>
 
-                <TextInput style={styles.inputLogin} placeholder=" CPF" />               
-                <TextInput style={styles.inputLogin} placeholder=" Senha" />
+                <TextInput style={styles.inputLogin} 
+                    placeholder=" CPF" 
+                    onChangeText={(cpf) => setCPF(cpf)}
+                />
 
+                <TextInput style={styles.inputLogin} 
+                    placeholder=" Senha"
+                    secureTextEntry={true}
+                    onChangeText={(password) => setPassword(password)}
+                />
+
+                
                 <Text style={styles.forgotPassword}>Esqueci a senha</Text>
             </View>
 
+            <Text>{"CPF ==> " + cpf}</Text>
+            <Text>{"Senha ==> " + password}</Text>
+
             <ButtonLogin 
-                onPress={() => navigation.navigate('Home')}
+                onPress={() => {
+                    
+                    if(cpf == '123456789' && password == '123456'){
+                        navigation.navigate('Home')
+                    } else {
+                        Alert.alert('CPF ou senha incorretos')
+                    }
+                }}
             />
             <ButtonCreateAccountSignIn
                 onPress={() => navigation.navigate('CreateAccount')}
