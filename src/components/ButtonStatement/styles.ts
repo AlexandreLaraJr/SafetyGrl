a687ad2d91699e020901f59a9d36f30a6eff281d
Matheus Ribeiro <email1001utilidades@gmail.com>
2021-12-08 14:39:38 -0300

    Alterado estilos e formatações.

--- src/components/ButtonStatement/styles.ts
+++ src/components/ButtonStatement/styles.ts
@@ -1,23 +1,25 @@
-import { StyleSheet } from 'react-native'
-import { theme } from '../../global/styles/theme'
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-    container: {
-        width: 200,
-        height: 40,
-        backgroundColor: theme.colors.primary,
-        borderRadius: 8,
-        flexDirection: 'row',
-        alignItems: 'center',
-        position: 'absolute', //deixa em cima ou em baixo
-        bottom: 30,
-        //shadowColor: theme.colors.primary,
-    },
-    titleButton: {
-        flex: 1,
-        color: 'white',
-        fontSize: 15,
-        textAlign: 'center',
-        fontWeight: 'bold',
-    },
-})
+  container: {
+    width: 200,
+    height: 40,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "white",
+    flexDirection: "row",
+    alignItems: "center",
+    position: "absolute", //deixa em cima ou em baixo
+    bottom: 30,
+    //shadowColor: theme.colors.primary,
+  },
+  titleButton: {
+    flex: 1,
+    color: "white",
+    fontSize: 15,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+});
