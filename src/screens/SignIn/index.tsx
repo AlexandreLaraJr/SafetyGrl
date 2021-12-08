7ce5e9d27306000d03007fb04fa18f85b96bb195
Matheus Ribeiro <email1001utilidades@gmail.com>
2021-12-08 15:23:18 -0300

    Adicionado tamanho máximo no campo CPF (login agora é 12345678900

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,87 +1,82 @@
-import React from "react"
-import { 
-    View, 
-    Text, 
-    Image,
-    StatusBar,
-    TextInput,
-    Alert
-} from 'react-native'
+import React from "react";
+import { View, Text, Image, StatusBar, TextInput, Alert } from "react-native";
 
-import 'react-native-gesture-handler';
+import { TextInputMask } from "react-native-masked-text";
+import "react-native-gesture-handler";
 import { useNavigation } from "@react-navigation/native";
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RootStackParamList} from '../RootStackPrams';
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../RootStackPrams";
 
-import IllustrationImgLogo from '../../assets/Logo_app.png'
-import IllustrationImgNameLogo from '../../assets/SafetyGrl2.png'
+import IllustrationImgLogo from "../../assets/Logo_app.png";
+import IllustrationImgNameLogo from "../../assets/SafetyGrl2.png";
 
-import { ButtonCreateAccountSignIn, ButtonIconFacebook, ButtonIconGoogle, ButtonLogin } from "../../components/ButtonIconSignIn"
-import { styles } from "./styles"
+import {
+  ButtonCreateAccountSignIn,
+  ButtonIconFacebook,
+  ButtonIconGoogle,
+  ButtonLogin,
+} from "../../components/ButtonIconSignIn";
+import { styles } from "./styles";
 
-type createAccountScreenProp = StackNavigationProp<RootStackParamList, 'CreateAccount'>;
+type createAccountScreenProp = StackNavigationProp<
+  RootStackParamList,
+  "CreateAccount"
+>;
 
-export function SignIn(){
+export function SignIn() {
+  const [cpf, setCPF] = React.useState("");
+  const [password, setPassword] = React.useState("");
 
-    const [ cpf, setCPF ] = React.useState('')
-    const [ password, setPassword ] = React.useState('')
+  const navigation = useNavigation<createAccountScreenProp>();
 
-    const navigation = useNavigation<createAccountScreenProp>();
+  return (
+    <View style={styles.container}>
+      <StatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
+        translucent
+      />
 
-    return(
-        <View style={styles.container}>
-            <StatusBar 
-                barStyle="dark-content"
-                backgroundColor="transparent"
-                translucent
-            />
+      <Image source={IllustrationImgLogo} style={styles.image} />
 
-            <Image
-            source={IllustrationImgLogo} 
-            style={styles.image}
-            />
-            
-            <Image 
-                source={IllustrationImgNameLogo}
-                style={styles.nameLogo} 
-            />
+      <Image source={IllustrationImgNameLogo} style={styles.nameLogo} />
 
-            <View style={styles.content}>
+      <View style={styles.content}>
+        <TextInputMask
+          type={"cpf"}
+          value={cpf}
+          style={styles.inputLogin}
+          placeholder=" CPF"
+          length={11}
+          onChangeText={(cpf) => setCPF(cpf)}
+        />
 
-                <TextInput style={styles.inputLogin} 
-                    placeholder=" CPF" 
-                    onChangeText={(cpf) => setCPF(cpf)}
-                />
+        <TextInput
+          style={styles.inputLogin}
+          placeholder=" Senha"
+          secureTextEntry={true}
+          onChangeText={(password) => setPassword(password)}
+        />
 
-                <TextInput style={styles.inputLogin} 
-                    placeholder=" Senha"
-                    secureTextEntry={true}
-                    onChangeText={(password) => setPassword(password)}
-                />
+        <Text style={styles.forgotPassword}>Esqueci a senha</Text>
+      </View>
+      <ButtonLogin
+        onPress={() => {
+          if (cpf == "123.456.789-00" && password == "123456") {
+            navigation.navigate("Home");
+          } else {
+            Alert.alert("CPF ou senha incorretos");
+          }
+        }}
+      />
+      <ButtonCreateAccountSignIn
+        onPress={() => navigation.navigate("CreateAccount")}
+      />
 
-                
-                <Text style={styles.forgotPassword}>Esqueci a senha</Text>
-            </View>
-
-            <ButtonLogin 
-                onPress={() => {
-                    
-                    if(cpf == '123456789' && password == '123456'){
-                        navigation.navigate('Home')
-                    } else {
-                        Alert.alert('CPF ou senha incorretos')
-                    }
-                }}
-            />
-            <ButtonCreateAccountSignIn
-                onPress={() => navigation.navigate('CreateAccount')}
-            />
-
-            <View style={styles.containerIcons}>
-                <ButtonIconFacebook />
-                <ButtonIconGoogle />
-            </View>
-            
-        </View>
-    )
+      <View style={styles.containerIcons}>
+        <ButtonIconFacebook />
+        <ButtonIconGoogle />
+      </View>
+    </View>
+  );
 }
