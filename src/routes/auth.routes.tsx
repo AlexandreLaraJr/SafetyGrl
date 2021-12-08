0864c4d83b04eb9871783ce146a2af07698b549f
Matheus Ribeiro <email1001utilidades@gmail.com>
2021-12-08 14:40:19 -0300

    N sei qq mudou, mas confia no pai q ta certo

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 
 import { createStackNavigator } from "@react-navigation/stack";
 
-import { CreateAccount } from '../screens/CreateAccount'
-import { SignIn } from '../screens/SignIn'
+import { CreateAccount } from "../screens/CreateAccount";
+import { SignIn } from "../screens/SignIn";
 import { Home } from "../screens/Home";
 import { User } from "../screens/User";
 import { Complaint } from "../screens/Complaint";
@@ -14,66 +14,71 @@ import { ChangePassword } from "../screens/ChangePassword";
 import { Settings } from "../screens/Settings";
 import { Notifications } from "../screens/Notifications";
 import { Sos } from "../screens/Sos";
+import { Gambiarra } from "../screens/Gambiarra";
 
 const { Navigator, Screen } = createStackNavigator();
 
-export function AuthRoutes( ) {
-    return(
-            <Navigator /* define a navegação*/ 
-                screenOptions={{ 
-                    headerShown: false,
-                    cardStyle: {
-                        backgroundColor: 'transparent',
-                    }
-                }}
-                initialRouteName="SignIn"
-            >
-                <Screen //telas disponiveis para navegação
-                    name="SignIn"
-                    component={SignIn}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="CreateAccount"
-                    component={CreateAccount}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Home"
-                    component={Home}
-                />
-                
-                <Screen //telas disponiveis para navegação
-                    name="User"
-                    component={User}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Complaint"
-                    component={Complaint}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Statements"
-                    component={Statements}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Statements2"
-                    component={Statements2}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="ChangePassword"
-                    component={ChangePassword}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Settings"
-                    component={Settings}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Notifications"
-                    component={Notifications}
-                />
-                <Screen //telas disponiveis para navegação
-                    name="Sos"
-                    component={Sos}
-                />
+export function AuthRoutes() {
+  return (
+    <Navigator /* define a navegação*/
+      screenOptions={{
+        headerShown: false,
+        cardStyle: {
+          backgroundColor: "transparent",
+        },
+      }}
+      initialRouteName="SignIn"
+    >
+      <Screen //telas disponiveis para navegação
+        name="SignIn"
+        component={SignIn}
+      />
+      <Screen //telas disponiveis para navegação
+        name="CreateAccount"
+        component={CreateAccount}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Home"
+        component={Home}
+      />
 
-            </Navigator>
-    )
+      <Screen //telas disponiveis para navegação
+        name="User"
+        component={User}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Complaint"
+        component={Complaint}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Statements"
+        component={Statements}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Statements2"
+        component={Statements2}
+      />
+      <Screen //telas disponiveis para navegação
+        name="ChangePassword"
+        component={ChangePassword}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Settings"
+        component={Settings}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Notifications"
+        component={Notifications}
+      />
+      <Screen //telas disponiveis para navegação
+        name="Sos"
+        component={Sos}
+      />
+
+      <Screen //telas disponiveis para navegação
+        name="Gambiarra"
+        component={Gambiarra}
+      />
+    </Navigator>
+  );
 }
