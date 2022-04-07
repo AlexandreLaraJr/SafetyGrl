b5f072ffc18f89f68ebe2f81291404d94760587d
MgRibeir0 <email1001utilidades@gmail.com>
2022-04-07 19:11:12 -0300

    Botão OK e Cancelar adicionados

---
+++ src/components/ButtonCancel/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text } from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { styles } from "./styles";
+
+export function ButtonOk({ ...rest }: RectButtonProps) {
+  return (
+    <RectButton style={styles.buttonStyle} {...rest}>
+      <Text style={styles.buttonText}>Cancelar</Text>
+    </RectButton>
+  );
+}
---
+++ src/components/ButtonCancel/styles.ts
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import { theme } from "../../global/styles/theme";
+
+export const styles = StyleSheet.create({
+  buttonStyle: {
+    width: "30%",
+    height: 45,
+    backgroundColor: theme.colors.primary,
+    borderRadius: 8,
+    alignItems: "center",
+    flexDirection: "row",
+    padding: 20,
+  },
+  buttonText: {
+    flex: 1,
+    color: "white",
+    fontSize: 14,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
+    fontWeight: "bold",
+  },
+});
