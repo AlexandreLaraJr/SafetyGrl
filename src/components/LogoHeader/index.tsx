d8ddbae3b877fe534f79f98444bbc9b9d79f2ff8
MgRibeir0 <email1001utilidades@gmail.com>
2022-04-08 01:42:58 -0300

    Adicionado verificação de tela, se for home, não pode voltar

--- src/components/LogoHeader/index.tsx
+++ src/components/LogoHeader/index.tsx
@@ -1,35 +1,31 @@
 import React from "react";
-import { Image, View } from 'react-native'
+import { Image, View } from "react-native";
 
-import IllustrationImgLogo from '../../assets/Logo_app.png'
-import IllustrationImgNameLogo from '../../assets/SafetyGrl2.png'
+import IllustrationImgLogo from "../../assets/Logo_app.png";
+import IllustrationImgNameLogo from "../../assets/SafetyGrl2.png";
 import { ButtonBack } from "../ButtonHome";
 
 import { useNavigation } from "@react-navigation/native";
-import {StackNavigationProp} from '@react-navigation/stack';
-import {RootStackParamList} from '../../screens/RootStackPrams';
+import { StackNavigationProp } from "@react-navigation/stack";
+import { RootStackParamList } from "../../screens/RootStackPrams";
 
-import { styles } from './styles'
+import { styles } from "./styles";
 type ScreenProp = StackNavigationProp<RootStackParamList>;
 
-export function LogoHeader(){
-    const navigation = useNavigation<ScreenProp>();
-
-    return(
-        <View style={styles.container}>
-            <ButtonBack
-                onPress={() => navigation.goBack()}
-            />
-            <View style={styles.containerLogo}>
-                <Image 
-                    style={styles.logo}
-                    source={IllustrationImgLogo}
-                />
-                <Image 
-                    style={styles.nameLogo}
-                    source={IllustrationImgNameLogo}
-                />
-            </View>
-        </View>
-    )
+export function LogoHeader() {
+  const navigation = useNavigation<ScreenProp>();
+  return (
+    <View style={styles.container}>
+      <ButtonBack
+        onPress={() => {
+          if (navigation.getState().routes[1].name == "Home") return;
+          navigation.goBack();
+        }}
+      />
+      <View style={styles.containerLogo}>
+        <Image style={styles.logo} source={IllustrationImgLogo} />
+        <Image style={styles.nameLogo} source={IllustrationImgNameLogo} />
+      </View>
+    </View>
+  );
 }
