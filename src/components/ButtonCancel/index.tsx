8852ec9392262ab4a8d300d45a357124d949c375
MgRibeir0 <email1001utilidades@gmail.com>
2022-04-08 01:42:25 -0300

    Corrigido nome do componente ButtonOk -> ButtonCancel

--- src/components/ButtonCancel/index.tsx
+++ src/components/ButtonCancel/index.tsx
@@ -3,7 +3,7 @@ import { Text } from "react-native";
 import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { styles } from "./styles";
 
-export function ButtonOk({ ...rest }: RectButtonProps) {
+export function ButtonCancel({ ...rest }: RectButtonProps) {
   return (
     <RectButton style={styles.buttonStyle} {...rest}>
       <Text style={styles.buttonText}>Cancelar</Text>
