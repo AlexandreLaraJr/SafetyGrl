4cc2b212c231066c2c61b3c74a4a73d7d8c4aa8a
MgRibeir0 <email1001utilidades@gmail.com>
2022-04-09 11:53:33 -0300

    Movido funções de database para uma nova pasta

---
+++ resources/userFunctions.tsx
@@ -0,0 +1,57 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import db from "../database/firebase";
+import { verifyCPFOnDb } from "../src/screens/CreateAccount/index";
+import {
+  isEmailValid,
+  isPasswordValid,
+  verifyEmailOnDb,
+} from "./verifications";
+
+export async function createUserDB(
+  name: string,
+  socialName: string | null,
+  email: string,
+  cpf: string,
+  telefone: string,
+  senha: string,
+  senha2: string
+): Promise<any> {
+  let result = 0;
+  cpf = cpf.replace(/[^\d]+/g, "");
+  telefone = telefone.replace(/[^\d]+/g, "");
+  if (senha != senha2) result = -1; // senhas diferentes
+  if (isPasswordValid(senha)) result = -2; // senha inválida.
+  if (isEmailValid(email)) result = -3; // email invalido
+  if (cpf.length != 11) result = -4; // cpf invalido
+  if (telefone.length < 11 || telefone.length > 11) result = -5; // telefone invalido
+  if (name.length < 3) result = -6; // nome muito curto
+  let user = {
+    name: name,
+    socialName: socialName || "",
+    email: email,
+    cpf: cpf,
+    telefone: telefone,
+    senha: senha,
+  };
+
+  if (await verifyCPFOnDb(user.cpf)) return -7; // cpf ja cadastrado
+  if (await verifyEmailOnDb(user.email)) return -8; // email ja cadastrado
+
+  await db.ref("users/" + cpf).set(user); // cadastra usuario no banco de dados
+  return { result, user };
+}
+
+export async function getUserFromDB() {
+  let cpf: any = AsyncStorage.getItem("cpf").then((value) => {
+    return value;
+  });
+  let user: any = [];
+  cpf = await cpf;
+  await db
+    .ref("users/" + cpf)
+    .once("value")
+    .then((snap: any) => {
+      user.push(snap.val());
+    });
+  return user;
+}
---
+++ resources/verifications.tsx
@@ -0,0 +1,40 @@
+const db = require("../database/firebase");
+
+export function isEmailValid(email: string) {
+  let reg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return reg.test(email) == false;
+}
+
+export function isPasswordValid(password: string) {
+  let reg = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+  return reg.test(password) == true;
+}
+
+export async function updatePasswordOnDB(cpf: string, password: string) {
+  await db.ref("users/" + cpf).update({ senha: password });
+}
+export async function verifyPasswordOnDB(
+  cpf: string,
+  password: string
+): Promise<any> {
+  return await db
+    .ref("users/" + cpf)
+    .once("value")
+    .then((snap: any) => {
+      if (snap.val().senha == password) {
+        return true;
+      } else return false;
+    });
+}
+export async function verifyEmailOnDb(email: string): Promise<boolean> {
+  let isOnDB: boolean | Promise<any> = false;
+  isOnDB = await db
+    .ref("users")
+    .once("value")
+    .then((snap) => {
+      return snap.forEach((child) => {
+        if (child.val().email == email) return true;
+      });
+    });
+  return isOnDB;
+}
