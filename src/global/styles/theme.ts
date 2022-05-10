f0a52857c060a5d5c284df5c0de4ee9faab5fc0a
MgRibeir0 <email1001utilidades@gmail.com>
2022-05-10 12:12:59 -0300

    Comentários com função de cada cor

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,12 +1,12 @@
-export const theme = { 
-    colors: {
-        background: '#FFCEAA',
-        heading: 'red', //cor do cabeçalho
-        primary: '#92A3F8',
-        blackopacity: '#3F3535',
-        secundaryBackgroung: '#f27d83',
-        footer: '#E11552',
-        title: '#515983',
-        datasBackgroung: '#F5CDD9'
-    }
-}
+export const theme = {
+  colors: {
+    background: "#FFCEAA",
+    heading: "red", //cor do cabeçalho
+    primary: "#92A3F8", //cor dos botões
+    blackopacity: "#3F3535", //textos
+    secundaryBackgroung: "#f27d83", //fundo dos containers
+    footer: "#E11552",
+    title: "#515983", //cor do texto de titulos
+    datasBackgroung: "#F5CDD9", //cor dos campos de texto de dados
+  },
+};
