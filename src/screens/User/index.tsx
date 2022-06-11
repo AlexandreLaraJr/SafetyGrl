20e25d82a17bb7af979be7378485a0f2d9b8b639
MgRibeir0 <email1001utilidades@gmail.com>
2022-06-10 21:40:49 -0300

    Adicionado os dados para visualização

--- src/screens/User/index.tsx
+++ src/screens/User/index.tsx
@@ -1,8 +1,10 @@
 import { useNavigation } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
-import React from "react";
+import React, { useEffect } from "react";
 import { Image, Text, View } from "react-native";
 import "react-native-gesture-handler";
+import { getLocalCPF } from "../../../resources/localCreds";
+import { getUserFromDB } from "../../../resources/userFunctions";
 import IllustrationUser from "../../assets/icone_user.png";
 import {
   ButtonAlterarSenha,
@@ -19,6 +21,15 @@ type ChangePasswordScreenProp = StackNavigationProp<
 
 export function User() {
   const navigation = useNavigation<ChangePasswordScreenProp>();
+  const [user, setUser]: any = React.useState();
+
+  const getData = async () => {
+    setUser(await getUserFromDB(await getLocalCPF()));
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <View style={styles.container}>
@@ -32,28 +43,41 @@ export function User() {
 
         <View style={styles.contentDatas}>
           <Text style={styles.datas}>Nome</Text>
-
           <View style={styles.contentPersonalDatas}>
-            <Text style={styles.personalDatas}>{/* Nome do usuário */}</Text>
+            <Text style={styles.personalDatas}>
+              {typeof user == "undefined"
+                ? console.log("Deu undef no user")
+                : user[0].name}
+            </Text>
           </View>
 
           <Text style={styles.datas}>E-mail</Text>
 
           <View style={styles.contentPersonalDatas}>
-            <Text style={styles.personalDatas}>{/* Email do usuário */}</Text>
+            <Text style={styles.personalDatas}>
+              {typeof user == "undefined"
+                ? console.log("Deu undef no user")
+                : user[0].email}
+            </Text>
           </View>
 
           <Text style={styles.datas}>CPF</Text>
 
           <View style={styles.contentPersonalDatas}>
-            <Text style={styles.personalDatas}>{/* CPF do usuário */}</Text>
+            <Text style={styles.personalDatas}>
+              {typeof user == "undefined"
+                ? console.log("Deu undef no user")
+                : user[0].cpf}
+            </Text>
           </View>
 
           <Text style={styles.datas}>Telefone</Text>
 
           <View style={styles.contentPersonalDatas}>
             <Text style={styles.personalDatas}>
-              {/* Telefone do usuário */}
+              {typeof user == "undefined"
+                ? console.log("Deu undef no user")
+                : user[0].telefone}
             </Text>
           </View>
         </View>
@@ -68,7 +92,6 @@ export function User() {
           <ButtonAlterarSenha
             onPress={() => navigation.navigate("ChangePassword")}
           />
-          
         </View>
       </View>
     </View>
--- src/screens/User/styles.ts
+++ src/screens/User/styles.ts
@@ -10,11 +10,11 @@ export const styles = StyleSheet.create({
   content: {
     backgroundColor: theme.colors.secundaryBackgroung,
     flex: 1,
-    marginTop: '30%',
+    marginTop: "30%",
     marginLeft: "8%",
     marginRight: "8%",
     marginBottom: "5%",
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 8,
     borderWidth: 2,
     borderColor: theme.colors.footer,
@@ -23,59 +23,55 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     alignItems: "center",
     justifyContent: "center",
-    top: '5%',
-    left: '-10%',
-    
+    top: "5%",
+    left: "-10%",
   },
   title: {
     fontSize: 22,
     fontWeight: "bold",
     color: theme.colors.title,
-    right: '-1%',
+    right: "-1%",
   },
   iconUser: {
     width: 45,
   },
   contentDatas: {
-    top: '10%',
+    top: "10%",
   },
   datas: {
     color: theme.colors.blackopacity,
     fontSize: 16,
     fontWeight: "bold",
-    marginTop: '1%',
-    marginBottom: '4%',
-    
+    marginTop: "1%",
+    marginBottom: "4%",
   },
   contentPersonalDatas: {
-    backgroundColor: 'white',
+    backgroundColor: "white",
     width: 290,
     height: 35,
     borderRadius: 8,
     borderWidth: 0.5,
-    marginBottom: '1%',
+    marginBottom: "1%",
     justifyContent: "center",
-    
   },
   personalDatas: {
     color: "black",
     fontSize: 15,
     padding: 5,
-    
-    
+    opacity: 0.4,
   },
 
   buttons: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: '30%' ,
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: "30%",
   },
 
   contentEdit: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: '30%' ,
-    marginRight: '10%',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: "30%",
+    marginRight: "10%",
   },
   datasEdit: {
     fontSize: 15,
